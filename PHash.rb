91f937c42bb97c7c2f4eebbcda19cafb8a1ff214
michael Vincent <michaelvincent2293@gmail.com>
2017-11-30 18:02:00 -0500

    add __pforall_hash__
    modif #given_by
    a utiliser dans les implementations des options possible(static, dynamique, adj, ...)

--- PHash.rb
+++ PHash.rb
@@ -20,7 +20,7 @@ module PRuby
   ############################################################
   module PHash
     def peach( opts = {}, &b )
-      __pforall__( self, false, opts, &b )
+      __pforall_hash__( self, false, opts, &b )
     end
 
     def peach_pair
@@ -34,6 +34,34 @@ module PRuby
     def preduce
 
     end
+
+    private
+    # Itere un bloc, en parallele, sur les differents elements d'un tableau.
+    #
+    # @option (see #peach)
+    # @param [Array] resultat Dans quel tableau doivent etre conserves les elements du resultat
+    # @param [Symbol] given_by :default si on applique le bloc sur un element <array>
+    # du genre [key,value], :pair si on applique sur deux variables |k,v|, ou uniquement :key ou :value pour la valeur
+    # @param b Le bloc a executer
+    #
+    # @return Le tableau initial si resultat.eql? self, sinon un autre tableau: voir les methodes publiques
+    #
+    # @raise [DBC::Failure] Divers conditions selon les options
+    #
+    def __pforall_hash__( resultat, given_by, opts = {}, &b )
+      return resultat if size == 0
+
+      method, opts = __analyser_arguments__( resultat, opts )
+
+      # On applique maintenant la methode d'iteration approprie a
+      # chacun des elements du tableau associatif, en yieldant au bloc recu en
+      # argument.
+      send method, *opts, given_by, &b
+
+      resultat
+    end
+
+
   end
 
   # On etend les module Hash avec les methodes ainsi definies.
